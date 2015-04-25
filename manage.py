b51d0a9e3754b93fabfa4abdad3b86d51b272ad0
AnastasiaFilatova <anastasia.n.filatova@gmail.com>
2015-04-25 11:05:50 +0200

    Modified settings for the project

--- manage.py
+++ manage.py
@@ -2,8 +2,11 @@
 import os
 import sys
 
+
+project_path = lambda *a: os.path.join(os.path.dirname(__file__), *a)
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UrlShortener.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UrlShortener.local")
 
     from django.core.management import execute_from_command_line
 
